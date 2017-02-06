1b80074d77d5ef4f3dfaf33d370fe834e431aa15
William Laroche <William Laroche>
2017-02-06 10:33:34 -0500

    Modified build options and placed hello world in root path

--- src/main/java/nacityfinder/Main.java
+++ src/main/java/nacityfinder/Main.java
@@ -5,7 +5,7 @@ import static spark.Spark.*;
 public class Main {
 
 	public static void main(String[] args) {
-		get("/hello", (req, rep) -> "Hello World");
+		get("/", (req, rep) -> "Hello World");
 	}
 
 }
