9324155a69ab2061686e99bdff314312b0458993
William Laroche <William Laroche>
2017-02-06 10:53:22 -0500

    Added port and static file setup

--- src/main/java/nacityfinder/Main.java
+++ src/main/java/nacityfinder/Main.java
@@ -5,6 +5,9 @@ import static spark.Spark.*;
 public class Main {
 
 	public static void main(String[] args) {
+	    port(Integer.valueOf(System.getenv("PORT")));
+	    staticFileLocation("/public");
+	    
 		get("/", (req, rep) -> "Hello World");
 	}
 
